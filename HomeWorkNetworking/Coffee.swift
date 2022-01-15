a8785ba48be74967bb4206bf0379b1995379a747
Dmitry <radiator211@mail.ru>
2022-01-15 23:16:16 +0700

    Work with networking

--- HomeWorkNetworking/Coffee.swift
+++ HomeWorkNetworking/Coffee.swift
@@ -10,4 +10,3 @@ import Foundation
 struct Coffee: Decodable {
     var file: String?
 }
-
--- HomeWorkNetworking/CoffeeCollectionViewController.swift
+++ HomeWorkNetworking/CoffeeCollectionViewController.swift
@@ -17,6 +17,7 @@ class CoffeeCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     // MARK: UICollectionViewDataSource
